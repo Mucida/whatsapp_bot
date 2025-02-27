4d2318c5ebae961f6d82cf00eb87ffe7825d391f
Mucida <lucas.mucida@gmail.com>
2025-02-27 16:14:17 -0300

    alterando porta

--- wtp.py
+++ wtp.py
@@ -79,5 +79,11 @@ def webhook():
     return str(resp)
 
 
+'''para rodar em producao'''
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+
+
+'''para rodar local'''
+#if __name__ == "__main__":
+#    app.run(port=5000, debug=True)
